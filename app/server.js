641aa9f15c4c6be3924c625c36d8598154cf3d25
jeanpan <beats.1213@gmail.com>
2016-09-13 17:08:36 +0800

    update routing

--- app/server.js
+++ app/server.js
@@ -4,6 +4,14 @@ var express = require("express");
 var app = express();
 var converter = require("./converter");
 
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.send('Welcome to Color Code Converter');
+});
+
 app.get("/rgbToHex", function(req, res) {
   var red   = parseInt(req.query.red, 10);
   var green = parseInt(req.query.green, 10);
@@ -22,4 +30,6 @@ app.get("/hexToRgb", function(req, res) {
   res.send(JSON.stringify(rgb));
 });
 
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
